3016df97090c1f44fb9e64649ae63bf263d2969e
Ananda Babu Gummadilli <Anand.Gummadilli@neudesic.com>
2025-04-18 15:57:02 +0530

    Added test cases for locators

--- tests2/LearnLocators.spec.ts
+++ tests2/LearnLocators.spec.ts
@@ -16,6 +16,27 @@ test('Findelement By xpath',async({})=>{
     await page2.goto('https://www.google.com');
     const txtSrch:Locator=await page2.locator("//textarea[@name='q']");
     await txtSrch.fill("India");
+    await page2.screenshot({path:'googlesearch.jpeg'});
+    const text1:Locator= await page2.locator('//div[contains(text(),"Google offered in:")]');
+    console.log("google text visible" + text1.isVisible);
     //browser.close();
 
+});
+
+test('Findelement By classname',async({})=>{
+    const browser:Browser=await chromium.launch({headless:false, channel:'chrome'});
+    const page2:Page=await browser.newPage();
+    await page2.goto('https://www.google.com');
+    const txtSrch:Locator=await page2.locator('textarea.gLFyf');
+    await txtSrch.fill("India");
+    
+});
+
+test('Findelement By text',async({})=>{
+    const browser:Browser=await chromium.launch({headless:false, channel:'chrome'});
+    const page2:Page=await browser.newPage();
+    await page2.goto('https://www.amazon.in');
+    const giftcards:Locator=await page2.getByText("Amazon Pay").first(); //xpath
+    await giftcards.click();
+    
 });
