77c6c1788f454a72b2a00520d4998a8f3391b3a2
Anand Gummadilli <anandg99@yahoo.com>
2025-04-26 06:36:59 +0530

    Added Actions related test cases

---
+++ tests2/Actions.spec.ts
@@ -0,0 +1,34 @@
+import {test,expect,Locator, Page, Browser} from '@playwright/test'
+import { BADHINTS } from 'dns';
+import { chromium,webkit,firefox } from 'playwright'
+
+test('Learn Mouse hover', async()=>{
+      const browser:Browser = await chromium.launch({headless:false, channel:'chrome'}); 
+        const page:Page= await browser.newPage();
+        await page.goto('https://www.spicejet.com');
+        await page.getByText('Add-ons').first().hover();
+        await page.getByText('Taxi').first().click();
+        await page.waitForTimeout(10000);
+           
+});
+
+test('Learn Double click', async()=>{
+    const browser:Browser = await chromium.launch({headless:false, channel:'chrome'}); 
+      const page:Page= await browser.newPage();
+      await page.goto('https://www.google.com');
+      const btnSignin:Locator=await page.locator("xpath=//a[@aria-label='Sign in']");
+      await btnSignin.dblclick(); //double click
+      await page.waitForTimeout(10000); //just wait for 10 secs
+         
+});
+
+
+test('Learn Right click', async()=>{
+    const browser:Browser = await chromium.launch({headless:false, channel:'chrome'}); 
+      const page:Page= await browser.newPage();
+      await page.goto('https://www.google.com');
+      const btnSignin:Locator=await page.locator("xpath=//a[@aria-label='Sign in']");
+      await btnSignin.first().click({ button: "right" }); //right click
+      await page.waitForTimeout(10000); //just wait for 10 secs
+         
+});
