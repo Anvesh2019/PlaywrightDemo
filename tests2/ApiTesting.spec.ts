a60dd17c82c6c1be1fc892ab42932ce6923c3291
Anand <anandg99@yahoo.com>
2026-06-29 16:49:49 +0530

    fixed API test case

--- tests2/ApiTesting.spec.ts
+++ tests2/ApiTesting.spec.ts
@@ -7,4 +7,10 @@ test("API Get call", async ({ request }) => {
   console.log(await response.status());
   console.log(await response.statusText());
   console.log(await response.headers());
+  await expect(response.status()).toBe(200);
+  const responseBody: any = await response.json();
+
+//expect(responseBody.name).toContain("doggie");
+  //await expect(responseBody).toBeTruthy("Hildegard");
+  expect(responseBody.some((item: any) => item.name === "doggie")).toBeTruthy();
 });
