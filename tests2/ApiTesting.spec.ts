59092b79ca57f6c759b1472c83aeba6793569c9a
Anand <anandg99@yahoo.com>
2026-06-29 18:06:03 +0530

    added more TCs on API

--- tests2/ApiTesting.spec.ts
+++ tests2/ApiTesting.spec.ts
@@ -14,3 +14,16 @@ test("API Get call", async ({ request }) => {
   //await expect(responseBody).toBeTruthy("Hildegard");
   expect(responseBody.some((item: any) => item.name === "doggie")).toBeTruthy();
 });
+
+
+test("API Verify Response", async ({ request }) => {
+  const response = await request.get("https://petstore.swagger.io/v2/pet/9223372036854001000");
+  //expect(response.ok()).toBeTruthy();
+  console.log(await response.json());
+  console.log(await response.status());
+  
+  //await expect(response.status()).toBe(200);
+  const responseBody: any = await response.json();
+
+  expect(responseBody.some((item: any) => item.id === "9223372036854001000")).toBeTruthy();
+});
