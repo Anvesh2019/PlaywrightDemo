fcf73921c111b0b37f56fd22352a4304a390d9f3
Anand <anandg99@yahoo.com>
2026-07-28 22:09:16 +0530

    Added TC

--- tests2/login.spec.ts
+++ tests2/login.spec.ts
@@ -10,3 +10,16 @@ test("Google test",async({page})=>{
  .toHaveTitle(/Google/);
 
 });
+
+test("search for anvesh",async({page})=>{
+
+ await page.goto("https://google.com");
+ await page.waitForTimeout(5000);   
+ await page.getByRole('combobox').fill('Anvesh');
+ 
+ await page.waitForTimeout(5000);   
+ await page.getByRole('combobox').press('Enter');
+ 
+ await page.waitForTimeout(5000);   
+ await expect(page).toHaveTitle(/Anvesh/);
+});
